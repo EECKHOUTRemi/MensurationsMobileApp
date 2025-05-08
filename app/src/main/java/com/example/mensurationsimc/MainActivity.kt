7726e80e96458b7da8bae32e27d88dcb1c3fc1ab
EECKHOUT Rémi <reeckhout@myges.fr>
2025-05-08 17:23:54 +0200

    feat: TopBar

--- app/src/main/java/com/example/mensurationsimc/MainActivity.kt
+++ app/src/main/java/com/example/mensurationsimc/MainActivity.kt
@@ -4,72 +4,85 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewModelScope
-import androidx.room.Room
-import com.example.mensurationsimc.database.AppDatabase
-import com.example.mensurationsimc.database.Profile
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.mensurationsimc.ui.theme.MensurationsIMCTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
-
-        lifecycleScope.launch {
-            val db = withContext(Dispatchers.IO) {
-                Room.databaseBuilder(
-                    applicationContext,
-                    AppDatabase::class.java,
-                    "profile-database"
-                )
-                    .fallbackToDestructiveMigration(true)
-                    .build()
-            }
-
-            setContent {
-                val viewModel = ProfileViewModel(db)
-                MensurationsIMCTheme {
-                    ProfileScreen(viewModel = viewModel)
+        setContent {
+            MensurationsIMCTheme(darkTheme = false) {
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) { innerPadding ->
+                    HomePage(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
 }
-
-class ProfileViewModel(database: AppDatabase) : ViewModel() {
-    private val profileDao = database.profileDao()
-
-    private val _profiles = MutableStateFlow<List<Profile>>(emptyList())
-    val profiles: StateFlow<List<Profile>> = _profiles
-
-    init {
-        loadProfiles()
-    }
-
-    private fun loadProfiles() {
-        viewModelScope.launch {
-            _profiles.value = profileDao.getAll()
-        }
-    }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Header(
+//    onMenuClick: () -> Unit,
+//    onProfileClick: () -> Unit,
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text="Mensuivi",
+                modifier = Modifier,
+                textAlign = TextAlign.Center,
+            )
+        },
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = "Menu",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable { //onMenuClick()
+                    },
+                tint = Color.White
+                )
+        },
+        actions = {
+//            Image(
+//                painter = painterResource(id = R.drawable.icon_profile),
+//            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+            )
+    )
 }
 
 @Composable
-fun ProfileScreen(viewModel: ProfileViewModel) {
-    val profiles by viewModel.profiles.collectAsState()
-
-    profiles.forEach { profile ->
-        Text(text = "Nom: ${profile.name}, Taille: ${profile.height}")
-    }
+fun HomePage(modifier: Modifier = Modifier) {
+    Header()
+    Text(
+        text = "Home Page",
+        modifier = modifier
+    )
 }
