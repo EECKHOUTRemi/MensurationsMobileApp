a64ee66c9bde40e72a2e75e0241a61b150c8f922
EECKHOUT Rémi <reeckhout@myges.fr>
2025-05-08 17:23:54 +0200

    feat: TopBar

--- app/src/main/java/com/example/mensurationsimc/MainActivity.kt
+++ app/src/main/java/com/example/mensurationsimc/MainActivity.kt
@@ -4,24 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mensurationsimc.ui.theme.MensurationsIMCTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MensurationsIMCTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+            MensurationsIMCTheme(darkTheme = false) {
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) { innerPadding ->
+                    HomePage(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,10 +47,47 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Header(
+//    onMenuClick: () -> Unit,
+//    onProfileClick: () -> Unit,
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text="Mensuivi",
+                modifier = Modifier,
+                textAlign = TextAlign.Center,
+            )
+        },
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = "Menu",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable { //onMenuClick()
+                    },
+                tint = Color.White
+                )
+        },
+        actions = {
+//            Image(
+//                painter = painterResource(id = R.drawable.icon_profile),
+//            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+            )
+    )
+}
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier) {
+    Header()
     Text(
-        text = "Hello $name!",
+        text = "Home Page",
         modifier = modifier
     )
 }
@@ -42,6 +96,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MensurationsIMCTheme {
-        Greeting("Android")
+        HomePage()
     }
 }
