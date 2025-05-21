763c862df2f2d5db8be71b8cc9aa8cc368f66bf7
EECKHOUT Rémi <reeckhout@myges.fr>
2025-05-21 16:51:55 +0200

    init: new branch

--- app/src/main/java/com/example/mensurationsimc/MainActivity.kt
+++ app/src/main/java/com/example/mensurationsimc/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MensurationsIMCTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android ",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
