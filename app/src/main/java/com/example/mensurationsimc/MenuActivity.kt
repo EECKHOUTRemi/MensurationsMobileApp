96b413f65250f20e982443457df335d0f379d277
EECKHOUT Rémi <reeckhout@myges.fr>
2025-05-24 23:06:36 +0200

    feat: functional menu icon

--- app/src/main/java/com/example/mensurationsimc/MenuActivity.kt
+++ app/src/main/java/com/example/mensurationsimc/MenuActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -22,42 +21,37 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MenuActivity : ComponentActivity() {
     val tag = "MENSUIVI"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(tag, "Main activity created")
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.light(Color.Transparent.toArgb(), White.toArgb()),
         )
         setContent {
+            val navController = rememberNavController()
             Scaffold(
                 topBar = {
-                    MainMenuScreen()
                     Header(
-                        onMenuClick = { /*  */ },
-                        onProfileClick = { /*  */ }
+                        navController = navController
                     )
                 },
                 modifier = Modifier
                     .fillMaxSize(),
             ) { innerPadding ->
-                Column {
-                    Text(
-                        text = "Main Menu",
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        fontSize = 24.sp,
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
+                MainMenuScreen(
+                    modifier = Modifier.padding(innerPadding),
+                    navController = navController
+                )
             }
         }
     }
@@ -89,10 +83,47 @@ class MenuActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun MenuNavHost(navController: NavHostController = rememberNavController(), modifier: Modifier) {
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable(
+            route = "home",
+            arguments = emptyList()
+        ) { innerPadding ->
+            Accueil(
+                modifier = Modifier
+            )
+        }
+        composable(
+            route = "menu",
+            arguments = emptyList() // If there are no arguments
+        ) {
+            MainMenuScreen(navController = navController)
+        }
+
+        composable(
+            route = "profile",
+            arguments = emptyList()
+        ) {
+            ProfileScreen(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(White)
+                    .padding(horizontal = 20.dp),
+                navController = navController
+            )
+        }
+
+
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainMenuScreen(modifier: Modifier = Modifier) {
+fun MainMenuScreen(modifier: Modifier = Modifier, navController: NavController) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -125,46 +156,3 @@ fun MenuItem(text: String) {
             .clickable { /* TODO: Navigate */ }
     )
 }
-
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun Header(
-//    onMenuClick: () -> Unit,
-//    onProfileClick: () -> Unit,
-//) {
-//    TopAppBar(
-//        title = {
-//            Text(
-//                text = "Mensuivi",
-//                modifier = Modifier.fillMaxWidth(),
-//                textAlign = TextAlign.Center,
-//                fontFamily = Inter,
-//                fontWeight = FontWeight.Black
-//            )
-//        },
-//        navigationIcon = {
-//            Icon(
-//                imageVector = Icons.Default.Menu,
-//                contentDescription = "Menu",
-//                modifier = Modifier
-//                    .padding(8.dp)
-//                    .clickable { onMenuClick() },
-//                tint = Color.Black
-//            )
-//        },
-//        actions = {
-//            Image(
-//                painter = painterResource(id = R.drawable.ic_launcher_background),
-//                contentDescription = "Profil",
-//                modifier = Modifier
-//                    .size(50.dp)
-//                    .padding(8.dp)
-//                    .clip(CircleShape)
-//                    .clickable { onProfileClick() }
-//            )
-//        },
-//        colors = TopAppBarDefaults.topAppBarColors(
-//            containerColor = White
-//        ),
-//    )
-//}
