96f99186e23109ed2a1ef2c8087bec3075166a36
EECKHOUT Rémi <reeckhout@myges.fr>
2025-05-26 00:21:58 +0200

    fix: @RequiresApi(Build.VERSION_CODES.O)

--- app/src/main/java/com/example/mensurationsimc/BmiActivity.kt
+++ app/src/main/java/com/example/mensurationsimc/BmiActivity.kt
@@ -1,11 +1,13 @@
 package com.example.mensurationsimc
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -21,6 +23,7 @@ import androidx.navigation.compose.rememberNavController
 
 class BmiActivity : ComponentActivity() {
     val tag = "MENSUIVI"
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i(tag, "Main activity created")
